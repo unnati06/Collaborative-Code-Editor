97b4ec1a3c59579df52da59108919858ddbfc316
Unnati <unnatisrivastava060@gmail.com>
2025-04-22 23:06:47 +0530

    client avatars

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import Avatar, { genConfig } from 'react-nice-avatar'
+import React, { useMemo } from 'react';
+import Avatar, { genConfig } from 'react-nice-avatar';
 
 const Clients = ({ username }) => {
-    const config = genConfig() 
-    console.log(username);
+    // Memoize the config based on username
+    const config = useMemo(() => genConfig(username), [username]);
+
     return (
-        <div className="client">
-           <Avatar className="w-16 h-16 ml-3" {...config} />
-            {/* <img src="" alt="user" /> */}
-            <span className="userName">{username}</span>
+        <div className="client flex flex-col items-center gap-2">
+            <Avatar 
+                className="w-16 h-16 rounded-full border-2 border-white shadow-lg transition-all hover:scale-110"
+                {...config} 
+            />
+            <span className="text-gray-200 font-medium text-sm">
+                {username}
+            </span>
         </div>
     );
 };
